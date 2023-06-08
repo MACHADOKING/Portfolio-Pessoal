9c5f67293b210dc5e598ef0521f2ffc51ce3d332
Julio Machado <jsmacbusiness@gmail.com>
2023-06-08 17:07:47 -0300

    First version - Portfólio

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Services } from "./components/Services";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
+import { Testimonials } from "./components/Testimonials";
+import Contact from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
